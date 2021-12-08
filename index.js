69681e0f7e7073a9e61a17e600dbc0221d30da89
Andersooncr <andersondocarmo.970@gmail.com>
2021-12-07 21:36:06 -0300

    First commit

--- index.js
+++ index.js
@@ -40,45 +40,7 @@ const flash = require('connect-flash')
     //routs
         app.use('/admin', admin);
 
-<<<<<<< HEAD
     //Others
         app.listen(9091,()=>{
             console.log("Server online")
         });
-=======
-    app.get('/',function(req,res){
-        Post.findAll({order: [['id', 'DESC']]}).then(function(posts){
-            res.render('home', {posts : posts})
-        }).catch(function(error){
-            res.send("There was an error"+ error)
-        })
-    });
-
-    app.get('/cad', function(req,res){
-        res.render('form')
-    });
-
-    app.get('/delete/:id',function(req,res){
-        Post.destroy({where : {'id': req.params.id}}).then(function(){
-            res.render('back')
-        }).catch(function(error){
-            res.send("There was an error"+ error)
-        })
-    });
-
-    app.post('/sended', function(req,res){
-        Post.create({
-            title: req.body.title,
-            contents: req.body.contents
-        
-        }).then(function(){
-            res.redirect('/')
-        }).catch(function(error){
-            res.send("There was an error"+ error)
-        })
-    });
-
-    app.listen(9091,function(){
-        console.log("Server online")
-    });
->>>>>>> b31bbde37d0b09756a5b06128ff2987b9ac7a497
